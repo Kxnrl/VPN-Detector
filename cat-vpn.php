fd7c04af340afe6575e88d11bf4501e2132df672
Kyle <kyle@kxnrl.com>
2018-07-08 18:41:36 +0800

    CloudFlare/AkamaiEdge and some shit CDN

--- cat-vpn.php
+++ cat-vpn.php
@@ -1,7 +1,7 @@
 <?php
 
 if(!isset($_GET['id']) || empty($_GET['id'])) {
-    echo "Invalid SteamId";
+    echo "Your IP: ". get_userip();
     die(404);
 }
 
@@ -31,21 +31,31 @@ if($result = $rds->query("SELECT * FROM k_antiproxy WHERE steamid=$id")) {
 
 function get_userip() {
     if(isset($_SERVER)) {
-        if(isset($_SERVER['HTTP_CF_CONNECTING_IP'])) {
+        if(isset($_SERVER['HTTP_TRUE_CLIENT_IP'])) {            // Akamai / CloudFlare Business
+            return $_SERVER['HTTP_TRUE_CLIENT_IP'];
+        } elseif(isset($_SERVER['HTTP_CF_CONNECTING_IP'])) {    // CloudFlare Pro
             return $_SERVER['HTTP_CF_CONNECTING_IP'];
         } elseif(isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-            return $_SERVER['HTTP_X_FORWARDED_FOR'];
+            if(is_array($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+                return $_SERVER['HTTP_X_FORWARDED_FOR'][0];     // shit CloudFront / shit AlibabaCloud / shit Tencent
+            }else {
+                return $_SERVER['HTTP_X_FORWARDED_FOR'];
+            }
         } elseif(isset($_SERVER['HTTP_CLIENT_IP'])) {
             return $_SERVER['HTTP_CLIENT_IP'];
-        } else {
+        } elseif(isset($_SERVER['REMOTE_ADDR'])) {
             return $_SERVER['REMOTE_ADDR'];
         }
     } else {
-        if(getenv('HTTP_X_FORWARDED_FOR')) {
+        if(getenv('HTTP_TRUE_CLIENT_IP')) {
+            return getenv('HTTP_TRUE_CLIENT_IP');
+        } elseif(getenv('HTTP_CF_CONNECTING_IP')) {
+            return getenv('HTTP_CF_CONNECTING_IP');
+        } elseif(getenv('HTTP_X_FORWARDED_FOR')) {
             return getenv('HTTP_X_FORWARDED_FOR');
         } elseif(getenv('HTTP_CLIENT_IP')) {
             return getenv('HTTP_CLIENT_IP');
-        } else {
+        } elseif(getenv('REMOTE_ADDR')) {
             return getenv('REMOTE_ADDR');
         }
     }
